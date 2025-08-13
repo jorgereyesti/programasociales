9285c12113cf38c4020717035593a5a189603083
tob-alvarez <tosal1099@gmail.com>
2025-08-13 09:43:31 -0300

    cambios de deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://estadisticas.smt.gob.ar:4321',
   headers: { 'Content-Type': 'application/json' },
   timeout: 5000,
 });
